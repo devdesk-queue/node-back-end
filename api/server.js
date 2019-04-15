f379529ef3ba3cd9ad3882cbb9f209f8638b2e58
Pav0l <pavol.karas@gmail.com>
2019-04-15 08:24:24 +0200

    Add error handler middleware for server errors

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const middleware = require('./middleware/middleware');
 const ticketsRouter = require('./routing/tickets');
+const { serverError } = require('./middleware/error');
 require('dotenv').config();
 
 // Define variables
@@ -10,10 +11,15 @@ const ticketsUrl = '/api/tickets';
 // Middleware
 middleware(app);
 
+// Default "home" route to check if the API is working
 app.get('/', (req, res) => {
   res.status(200).json('API is live');
 });
 
+// Routes
 app.use(ticketsUrl, ticketsRouter);
+
+// Error handler middleware
+app.use(serverError);
 
 module.exports = app;
