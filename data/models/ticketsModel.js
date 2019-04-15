800e16de8e82766cb72c4a04fc93f7aef0050c99
Pav0l <pavol.karas@gmail.com>
2019-04-15 11:18:14 +0200

    Comment getTicketsWithCats function

--- data/models/ticketsModel.js
+++ data/models/ticketsModel.js
@@ -13,13 +13,19 @@ async function getTicketsWithCats() {
   const categories = await getAllCategories();
 
   allTickets.map(ticket => {
+    // create new key:value pair for every ticket
     ticket.categories = relTable
+      // filter the value based on matching ticket_id
       .filter(row => row.ticket_id === ticket.ticket_id)
+      // map over every filtered category object from 'categorized_tickets' table
       .map(item => {
+        // find the category from 'categories' table that matches category_id
         const catName = categories.find(a => {
           return a.category_id == item.category_id;
         });
+        // replace the item object with category name
         item = catName.category;
+        // return only the category name for every ticket
         return item;
       });
   });
