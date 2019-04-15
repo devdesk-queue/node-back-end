6e730c03be50c64dcf51225438799397f72ff433
macjabeth <macbethjonathan@gmail.com>
2019-04-15 14:09:27 -0400

    fix default configuration

--- config/default.js
+++ config/default.js
@@ -1,10 +1,12 @@
 module.exports = {
-  knex: { directory: './data/migrations' },
-  seeds: { directory: './data/seeds' },
-  pool: {
-    afterCreate: (conn, done) => {
-      // I believe this is necessary for cascading
-      conn.run('PRAGMA foreign_keys = ON', done);
+  knex: {
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    pool: {
+      afterCreate: (conn, done) => {
+        // I believe this is necessary for cascading
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     }
   }
 };
