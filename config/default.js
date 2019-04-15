03a2ccc0875fea87e43bc7afc008c71971fa4f5f
macjabeth <macbethjonathan@gmail.com>
2019-04-15 13:12:36 -0400

    update knex configuration with config library

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  knex: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+  pool: {
+    afterCreate: (conn, done) => {
+      // I believe this is necessary for cascading
+      conn.run('PRAGMA foreign_keys = ON', done);
+    }
+  }
+};
