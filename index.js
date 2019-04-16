6ee9186746a8b5a2f8f264b9b863c263b2599d08
Carlos Lantigua <clantigua954@gmail.com>
2019-04-16 11:58:41 -0400

    added port to env file

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 const server = require('./api/server');
 
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT;
 
 server.listen(port, () => console.log(`=== Listening on port ${port} in ${server.settings.env} mode ===`));
