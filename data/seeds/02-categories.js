00fb6d3c16373aae2c562de6defacc7e7d86e1c4
macjabeth <macbethjonathan@gmail.com>
2019-04-16 00:21:22 -0400

    update & add seed data

--- data/seeds/02-categories.js
+++
@@ -1,8 +0,0 @@
-exports.seed = function(knex) {
-  return knex('categories').insert([
-    { category_id: 1, category: 'React' },
-    { category_id: 2, category: 'Administration' },
-    { category_id: 3, category: 'ISA' },
-    { category_id: 4, category: 'Other' }
-  ]);
-};
