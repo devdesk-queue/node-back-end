65eb5939b4a1b088551733947b818ce0c7af7b7b
Carlos Lantigua <clantigua954@gmail.com>
2019-04-16 13:03:20 -0400

    adding stuff back in less the dotenv

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 // require('dotenv').config();
-const express = require('express');
 
-const server = express();
-
-server.get('/', (req, res) => {
-  res.status(200).json({ message: 'it sorta works' });
-});
-
-// const server = require('./api/server');
-const port = process.env.PORT || 9000;
-
-
+const server = require('./api/server');
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log('=== Listening on port %d in %s mode ===', port, server.settings.env);
