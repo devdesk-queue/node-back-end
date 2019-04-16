91f1125c042bca4ff8401114fb2c52918a147ea1
Carlos Lantigua <clantigua954@gmail.com>
2019-04-16 11:30:37 -0400

    attempting something crazy

---
+++ Procfile
@@ -0,0 +1,2 @@
+heroku run knex migrate:latest -a devdeskqueue
+web: node index.js
--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const server = require('./api/server');
 
 const port = process.env.PORT || 9000;
 
-server.listen(port, () => console.log(`=== Listening on port ${port} in ${server.settings.env} mode ===`, server.settings.env));
+server.listen(port, () => console.log(`=== Listening on port ${port} in ${server.settings.env} mode ===`));
