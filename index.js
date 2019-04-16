a0c85f5e63befe15dec5f38907b80f3e1e4c94e0
Carlos Lantigua <clantigua954@gmail.com>
2019-04-16 13:17:14 -0400

    added hard code 9000 into port call

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // require('dotenv').config();
 
 const server = require('./api/server');
-const port = process.env.PORT;
+const port = process.env.PORT || 9000;
 
 server.listen(port, () => {
   console.log('=== Listening on port %d in %s mode ===', port, server.settings.env);
