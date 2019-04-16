ced4ff168468d183fd7227981d574f6d0c3bdeee
Carlos Lantigua <clantigua954@gmail.com>
2019-04-16 12:38:00 -0400

    testing basic route

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 // Handle async catch errors from one source.
-require('express-async-errors');
-
+// require('express-async-errors');
 const server = require('express')();
-
-require('../middleware')(server);
-
-require('../routes')(server);
+// require('../middleware')(server);
+// require('../routes')(server);
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'It works' });
+});
 
 module.exports = server;
