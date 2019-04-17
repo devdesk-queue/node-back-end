87233e1a5d616c852767f807dc5470a4a824ace9
macjabeth <macbethjonathan@gmail.com>
2019-04-17 16:51:39 -0400

    update ticket foreign keys & collate migration tables

--- data/migrations/003-createCategories.js
+++
@@ -1,7 +0,0 @@
-exports.up = knex =>
-  knex.schema.createTable('categories', table => {
-    table.increments();
-    table.string('name', 128).notNullable().unique();
-  });
-
-exports.down = knex => knex.schema.dropTableIfExists('categories');
