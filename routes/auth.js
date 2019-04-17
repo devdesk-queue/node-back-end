b69443ff0abd93115a32e95b322f0550887490d2
macjabeth <macbethjonathan@gmail.com>
2019-04-16 22:46:14 -0400

    update auth route validation & add checks for email/username uniqueness

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,52 @@
 const Joi = require('joi');
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-const userDB = require('../models/users');
+const Users = require('../models/users');
 const validate = require('../middleware/validate');
 const generateToken = require('../middleware/token');
 
 const schema = (creds, register) => {
-  const schema = Joi.object().keys({
-    email: register ? Joi.string().email().max(255).required() : null,
+  let schema = {
     username: Joi.string().max(255).required(),
     password: Joi.string().max(255).required()
+  };
+
+  if (register) schema = Object.assign(schema, {
+    email: Joi.string().email().max(255).required()
   });
+
   return Joi.validate(creds, schema);
 };
 
-router.post('/register', validate(schema, true), async ({ body: newUser }, res) => {
-  newUser.password = bcrypt.hashSync(newUser.password, 10);
-  const [id] = await userDB.add(newUser);
-  // eslint-disable-next-line no-unused-vars
-  const [{password, ...user}] = await userDB.get(id);
-  user.admin = Boolean(user.admin);
-  res.status(201).json(user);
-});
+router.post(
+  '/register',
+  validate(schema, true),
+  async ({ body: newUser }, res) => {
+    let [existingUser] = await Users.filter({ username: newUser.username });
+    if (existingUser) return res.status(405).json({
+      message: 'That username has already been taken.'
+    });
+
+    [existingUser] = await Users.filter({ email: newUser.email });
+    if (existingUser) return res.status(405).json({
+      message: 'There is an account with that email already.'
+    });
+
+    newUser.password = bcrypt.hashSync(newUser.password, 10);
+
+    const [id] = await Users.add(newUser);
+    // eslint-disable-next-line no-unused-vars
+    const [{ password, ...user }] = await Users.get(id);
+    const token = generateToken(user);
+
+    res.status(201).json({ user, token });
+  }
+);
 
 router.post('/login', validate(schema), async ({ body: creds }, res) => {
   const { username, password } = creds;
 
-  const [user] = await userDB.filter({ username });
+  const [user] = await Users.filter({ username });
   if (user && bcrypt.compareSync(password, user.password)) {
     const token = generateToken(user);
     res.status(200).json({
